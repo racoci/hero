668849262eb4c2bef13250e9ac85079a4d770b70
Lucas Racoci <racoci.0@gmail.com>
2020-03-25 00:40:10 -0300

    Updating method to create incident and created method to delete it

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -14,6 +14,31 @@ module.exports = {
             value,
             ong_id,
         });
-        return response.json({id})
+        return response.json({
+            id,
+            title,
+            description,
+            value,
+            ong_id,});
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+        const incident = await connection('incidents').where('id', id).select('ong_id').first()
+        if(!incident) {
+            return response.status(404).json({
+                error: `No resource found with id ${id}`
+            })
+        }
+        if(incident.ong_id !== ong_id) {
+            return response.status(401).json({
+                error: "Operation not permitted, you cannot delete an incident that is now from your own"
+            })
+        }
+
+        await connection('incidents').where('id', id).delete()
+
+        return response.status(204).send();
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,5 +11,6 @@ routes.post('/ongs', OngController.create);
 
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
