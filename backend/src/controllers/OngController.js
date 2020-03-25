ca64fadd89d643ff962d6d3c845ebf3ac204c9b6
Lucas Racoci <racoci.0@gmail.com>
2020-03-24 23:56:52 -0300

    Refactoring create and index methods to OngController

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,31 @@
+const connection = require("../database/connection");
+const crypto = require('crypto');
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*')
+        return response.json(ongs)
+    },
+    async create(request, response) {
+        const resp = {
+            params: {
+                query: request.query,
+                route: request.params
+            },
+            body: request.body
+        }
+        console.log(resp)
+        const {name, email, whatsapp, city, uf} = resp.body
+        const id = crypto.randomBytes(6).toString('BASE64');
+        const newRow = {
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        }
+        await connection('ongs').insert(newRow)
+        return response.json(newRow)
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,10 @@
 const express = require('express')
-const connection = require('./database/connection')
-const crypto = require('crypto');
+
+const OngController = require("./controllers/OngController")
 
 const routes = express.Router();
 
-routes.get('/ongs', async(request, response) => {
-    const ongs = await connection('ongs').select('*')
-    return response.json(ongs)
-})
-
-routes.post('/ongs', async (request, response) => {
-    const resp = {
-        params: {
-            query: request.query,
-            route: request.params
-        },
-        body: request.body
-    }
-    console.log(resp)
-    const {name, email, whatsapp, city, uf} = resp.body
-    const id = crypto.randomBytes(6).toString('BASE64');
-    const newRow = {
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    }
-    await connection('ongs').insert(newRow)
-    return response.json(newRow)
-})
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
