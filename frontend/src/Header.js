8358fd1dce2087605a079b2e4a62a2e03bfb0689
Lucas Racoci <racoci.0@gmail.com>
2020-03-25 23:00:44 -0300

    Improved parameter passing to React component

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-function Header(props) {
+function Header({title}) {
     return(
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
