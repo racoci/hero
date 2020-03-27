953f03c3d007ae9e25e33e0c0ea742ae31dd233b
Lucas Racoci <racoci.0@gmail.com>
2020-03-27 19:49:23 -0300

    Starting example with routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+function Incidents() {
+    return (
+        <View style={
+            { 
+                flex: 1, 
+                alignItems: 'center', 
+                justifyContent: 'center' 
+            }
+        }>
+            <Text>Incidents</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Incidents" component={Incidents} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+function App() {
+    return <Routes />
+}
+
+export default App;
